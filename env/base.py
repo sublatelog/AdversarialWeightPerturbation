688446f0a15bfe3eeac7c3306ffa59ecb5712df5
sublatelog <blueganz@gmail.com>
2022-04-16 14:10:51 +0900

    Update base.py

--- env/base.py
+++ env/base.py
@@ -5,13 +5,27 @@
 !cat /etc/issue　
 # Ubuntu 18.04.5 LTS \n \l
 
+
 # GPUの利用状況確認
 !nvidia-smi
 
 
+# pythonのバージョンでぇんこう
+# Choose one of the given alternatives:
+!sudo update-alternatives --config python3
+
+# This one used to work but now NOT(for me)!
+# !sudo update-alternatives --config python
+
+# Check the result
+!python3 --version
+
+# Attention: Install pip (... needed!)
+!sudo apt install python3-pip
 
 
+which python # /usr/local/bin/python
 
 
-
-
+# python　一覧
+ls /usr/bin/ | grep python
